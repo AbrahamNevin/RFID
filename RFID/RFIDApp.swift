9b2bd3552be802f0fe4abe1bb0681bdbc19ebb06
AbrahamNevin <nevinabr025@gmail.com>
2026-05-15 12:18:04 +0530

    Initial Commit

---
+++ RFID/RFIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  RFIDApp.swift
+//  RFID
+//
+//  Created by Nevin Abraham on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct RFIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
